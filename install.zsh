78cecc0c4c21a305d970ccc7bc1674667a50f9de
Matthew de Rooy <matt@derooy.com.au>
2014-03-18 19:40:11 +1000

    zsh installation

--- install.zsh
+++ install.zsh
@@ -1,5 +1,5 @@
 echo 'grabbing default oh my zsh ---------------'
-curl -L http://install.ohmyz.sh | sh
+curl -L http://install.ohmyz.sh | zsh
 echo 'grabbing .zshrc ----------------'
 if [ ! -f ~/.zshrc ]; then
     echo "zshrc not found"
@@ -7,6 +7,6 @@ else
 	echo "backing up to ~/.zshrc.backup"
 	yes | mv ~/.zshrc ~/.zshrc.backup
 fi
-wget 'https://raw.github.com/derooy/dotfiles/master/.zshrc' -O ~/.zshrc 
+wget 'https://raw.github.com/derooy/dotfiles/master/templates/.zshrc' -O ~/.zshrc 
 echo 'sourcing zsh'
 source ~/.zshrc
