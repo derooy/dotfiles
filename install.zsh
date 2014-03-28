04206527a25a1734c7ca5aa38e440312a7381c38
Matthew de Rooy (lapis) <matt@derooy.com.au>
2014-03-29 01:14:45 +1000

    colors

--- install.zsh
+++ install.zsh
@@ -1,16 +1,16 @@
-echo '---- Grabbing default ohmyzsh if not installed. ----'
+echo '\e[1;34m---- Grabbing default ohmyzsh if not installed. ----\e[0m'
 curl -L http://install.ohmyz.sh | zsh
-echo '---- Backup .zshrc ----'
+echo '\e[1;34m---- Backup .zshrc ----\e[0m'
 if [ ! -f ~/.zshrc ]; then
     echo "zshrc not found"
 else
 	echo "backing up to ~/.zshrc.backup"
 	yes | mv ~/.zshrc ~/.zshrc.backup
 fi
-echo '---- wGet the template from web ----'
+echo '\e[1;34m---- wGet the template from web ----\e[0m'
 wget 'https://raw.github.com/derooy/dotfiles/master/templates/.zshrc' -O ~/.zshrc 
-echo '---- Source the .zshrc ----'
+echo '\e[1;34m---- Source the .zshrc ----\e[0m'
 source ~/.zshrc
-echo '---- Installation Complete ----'
-echo 'Please relog =3 to enable zsh'
+echo '\e[1;34m---- Installation Complete ----\e[0m'
+echo '\e[1;34mPlease relog =3 to enable zsh\e[0m'
 sudo chsh -s `which zsh`
