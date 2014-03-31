47bd1677301d01ad88d9e6effb51424ff846115c
Matthew de Rooy <matt@derooy.com.au>
2014-04-01 01:37:37 +1000

    apt-get alias'

--- install.zsh
+++ install.zsh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 echo '\e[1;34m---- Grabbing default ohmyzsh if not installed. ----\e[0m'
 curl -L http://install.ohmyz.sh | zsh
 echo '\e[1;34m---- Backup .zshrc ----\e[0m'
@@ -11,6 +13,8 @@ echo '\e[1;34m---- wGet the template from web ----\e[0m'
 wget 'https://raw.github.com/derooy/dotfiles/master/templates/zshrc' -O ~/.zshrc 
 echo '\e[1;34m---- Source the .zshrc ----\e[0m'
 source ~/.zshrc
+echo '\e[1;34m---- Source the .zshrc ----\e[0m'
 echo '\e[1;34m---- Installation Complete ----\e[0m'
 echo '\e[1;34mPlease relog =3 to enable zsh\e[0m'
 sudo chsh -s $(which zsh)
+
