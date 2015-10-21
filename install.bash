b9c197f1798899ac8116b0e8cf16df2d25d54efc
derooy <matt@derooy.com.au>
2015-10-22 01:55:50 +1000

    Update install.bash

--- install.bash
+++ install.bash
@@ -1,34 +1,33 @@
 #!/bin/bash
 #remove previous install
-cd ~/
+cd ~/ # so im not in .bash_it folder to begin with
 rm -rf ~/.bash_it
-rm ~/.bashrc.bak
+rm ~/.bashrc.bak # to stop interaction with (are you sure? y/n)
 #install or update of bash-it
-git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it #because not possible to fetch depth=1 safely
 cd ~/.bash_it
 #install bash_it
 ./install.sh
-source ~/.bashrc
 #set theme to zork
 sed -i '/export BASH_IT_THEME=/ s/bobby/zork/' ~/.bashrc
-source ~/.bashrc
+source ~/.bashrc #so new theme and can get plugins etc.
 cd ~/
 #set my default plugins
 plugins=(dirs base explain extract fasd git history nginx projects hub ssh)
 for i in "${plugins[@]}"
 do
-        bash-it enable plugin $i
+	bash-it enable plugin $i
 done
 #set my default alias'
 aliases=(apt git vim vagrant docker)
 for i in "${aliases[@]}"
 do
-        bash-it enable alias $i
+	bash-it enable alias $i
 done
 #set my default completions
 completions=(defaults dirs git hub projects ssh vagrant)
 for i in "${completions[@]}"
 do
-        bash-it enable completion $i
+	bash-it enable completion $i
 done
 #get custom alias' completions and plugins from github
