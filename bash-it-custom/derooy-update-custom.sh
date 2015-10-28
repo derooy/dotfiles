f6973aac0719330e06881d63bc887bd091554d54
Matthew de Rooy <matt@derooy.com.au>
2015-10-28 15:50:45 +1000

    update custom from terminal

---
+++ bash-it-custom/derooy-update-custom.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# make sure bash-it and dotfiles is correct
+if [[ ! -d ~/.bash_it && ! -d ~/.dotfiles ]]; then
+  # Control will enter here if $DIRECTORY exists.
+  echo "bash_it and dotfiles don't exist"
+  exit 1
+fi
+
+# pull dotfiles
+echo "updating dotfiles"
+cd ~/.dotfiles
+git add --all
+git commit -a -m "update custom from terminal"
+git pull
+git push
+
+echo "removing custom from bash-it"
+cd ~/.bash_it/custom/
+find ! -name 'example.bash' -type f -exec rm -f {} +
+
+echo "updating aliases into bash-it"
+FILES=~/.dotfiles/bash-it-custom/*
+for f in $FILES
+do
+filename=$(basename "$f")
+echo ln -s "$f" "~/.bash_it/custom/$filename"
+done
+
+echo "source the new bash-it"
+source ~/.bashrc
