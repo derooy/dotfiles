10dbf9a0ab15b3441a7e9864a163fffe7f21eedc
Matthew de Rooy <matt@derooy.com.au>
2015-10-28 16:32:48 +1000

    update custom from terminal

--- bash-it-custom/alias.bash
+++ bash-it-custom/alias.bash
@@ -1,29 +1,29 @@
+# bash-it
 alias derooy-update-custom="bash ~/.bash_it/custom/derooy-update-custom.sh"
-#list
+# random
+alias c='clear'
+alias q='exit'
+alias f='sudo find / -name'
+alias ds='du -hs * | sort -h'
 alias l='ls -lh'
+alias ls='ls -lAh'
+# git
 alias gs='git status'
 alias gc='git commit -v -a'
 alias ga='git add .'
-#screen
+# screen
 alias sls="screen -ls" #list screens
 alias sd="screen -dmS" #screen in daemon mode
 alias sr="screen -x" #connect to screen w/ multi allowed
 alias ss="screen -S" #create screen with name
 alias slsa='sudo ls -laR /var/run/screen/'
-#zsh
-alias c='clear'
-alias q='exit'
-alias f='sudo find / -name'
-alias ds='du -hs * | sort -h'
-alias rm='/bin/rm' #undo rm -i from plugin
-alias ln='echo "ln -s {source} {destination}"; ln -s'
-
-#apt-get
+# apt
 alias in='sudo apt-get install'
 alias se='sudo apt-cache search'
 alias po='sudo apt-cache policy'
 alias upt='sudo apt-get update'
 alias upg='sudo apt-get upgrade'
+alias ppal='grep ^ /etc/apt/sources.list /etc/apt/sources.list.d/*'
 function ppa() {
 	sudo add-apt-repository ppa:$@
 }
@@ -32,4 +32,3 @@ function ppa() {
 alias vp='vagrant provision'
 alias vu='vagrant up'
 
-alias ppal='grep ^ /etc/apt/sources.list /etc/apt/sources.list.d/*'
