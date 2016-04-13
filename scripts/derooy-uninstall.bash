16a0d0e17f1bc34ae287424173b1584c0b0d6df6
derooy <matt@derooy.com.au>
2016-04-13 13:57:53 +1000

    Update derooy-uninstall.bash

--- scripts/derooy-uninstall.bash
+++ scripts/derooy-uninstall.bash
@@ -5,7 +5,7 @@
 # if bash_it exists; run bash-it uninstall
 # remove bash-it clone
 # --------------------------------
-rm ~/.inputrc 2> /dev/null
-rm -rf ~/.dotfiles 2> dev/null
+rm ~/.inputrc
+rm -rf ~/.dotfiles
 ~/.bash_it/uninstall.sh
 rm -rf ~/.bash_it
