8cd3a4ecc6df56e5b7b304771c13134f094281e0
derooy <matt@derooy.com.au>
2016-04-13 13:28:05 +1000

    Update install.bash

--- install.bash
+++ install.bash
@@ -1,6 +1,5 @@
 #!/bin/bash
 # -----------------------------------------------
-# 1. remove previous install, because you can't pull from a depth=1 clone
 # 2. depth 1 clone bash it
 # 3. run bash_it install
 # 4. set theme to zork
@@ -11,44 +10,43 @@
 # 9. git push.default simple
 # 10. url.ssh://git@github.com/.insteadOf https://github.com/
 # -----------------------------------------------
-#remove previous install
-cd ~/ # so im not in .bash_it folder to begin with
-rm -rf ~/.bash_it
-rm ~/.bashrc.bak # to stop interaction with (are you sure? y/n)
+# settings
+plugins=(alias-completion base explain)
+aliases=()
+completions=(defaults git ssh vagrant git_flow_avh)
+# -----------------------------------------------
 #install or update of bash-it
 git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it #because not possible to fetch depth=1 safely
-cd ~/.bash_it
-#install bash_it
-./install.sh
+bash ~/.bash_it/install.sh
 #set theme to zork
 sed -i '/export BASH_IT_THEME=/ s/bobby/zork/' ~/.bashrc
 source ~/.bashrc #so new theme and can get plugins etc.
 cd ~/
+# unset all plugins, alias' and compltetions
+bash-it disable plugin all
+bash-it disable alias all
+bash-it disable completion all
+
 #set my default plugins
-plugins=()
 for i in "${plugins[@]}"
 do
 	bash-it enable plugin $i
 done
 #set my default alias'
-aliases=()
 for i in "${aliases[@]}"
 do
 	bash-it enable alias $i
 done
 #set my default completions
-completions=(defaults git ssh vagrant git_flow_avh)
 for i in "${completions[@]}"
 do
 	bash-it enable completion $i
 done
+
 #get custom alias' completions and plugins from github
-git clone https://github.com/derooy/dotfiles.git ~/.dotfiles
+git clone --depth=1 https://github.com/derooy/dotfiles.git ~/.dotfiles
 cd ~/.dotfiles/bash-it-custom
 user=$(whoami)
-echo "removing custom from bash-it"
-cd ~/.bash_it/custom/
-find ! -name 'example.bash' -type l -exec rm -f {} +
 
 echo "updating aliases into bash-it"
 FILES=~/.dotfiles/bash-it-custom/*
@@ -59,6 +57,7 @@ echo ln -s "$f" "/home/$user/.bash_it/custom/$filename"
 ln -s "$f" "/home/$user/.bash_it/custom/$filename"
 done
 echo "tab completion case insenstive"
+echo "\$include /etc/inputrc" > ~/.inputrc
 echo "set completion-ignore-case On" > ~/.inputrc
 git config --global push.default simple
 git config --global url.ssh://git@github.com/.insteadOf https://github.com/
