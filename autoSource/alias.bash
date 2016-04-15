a4dbe196d64615bf8f0238f516f9b73b3ca7bf0d
Matthew de Rooy <matt@derooy.com.au>
2016-04-15 14:07:25 +1000

    auto commit

--- autoSource/alias.bash
+++ autoSource/alias.bash
@@ -3,7 +3,7 @@ alias derooy-install="bash ~/.dotfiles/scripts/derooy-install.bash"
 alias derooy-uninstall="bash ~/.dotfiles/scripts/derooy-uninstall.bash"
 alias derooy-reinstall="bash ~/.dotfiles/scripts/derooy-reinstall.bash"
 alias derooy-authorized-keys-github="bash ~/.dotfiles/scripts/derooy-authorized-keys-github.bash"
-alias derooy-upgrade-dofiles="dotfiles && gl && reload"
+alias derooy-upgrade-dotfiles="dotfiles && gl && reload"
 alias derooy-upgrade-bash-it="bash-it update"
 # directories
 alias ..='cd ..'
