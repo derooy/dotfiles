559cbd91421db82225f988a1f4c5946f8971c19c
Matthew de Rooy <matt@derooy.com.au>
2016-10-25 22:34:35 +1000

    auto commit

--- autoSource/path.bash
+++ autoSource/path.bash
@@ -1,6 +1,6 @@
 export EDITOR='vim'
 
-export GOPATH=$HOME/gocode
+export GOPATH=$HOME/go
 export PATH=$PATH:$GOPATH/bin
 export PATH=$PATH:/usr/local/go/bin
 export PATH=$PATH:/usr/local/go_appengine
