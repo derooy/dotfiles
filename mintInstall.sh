0be175ab0451a7eca2e5c61b5eedfc01607c6fe7
Matthew de Rooy <matt@derooy.com.au>
2016-10-25 22:00:54 +1000

    auto commit

--- mintInstall.sh
+++ mintInstall.sh
@@ -6,3 +6,4 @@ sudo apt-get autoremove
 sudo apt-get -y install terminator vim
 cd ~/
 rmdir Documents/ Pictures/ Public/ Templates/ Videos/ Music/
+git config --global url.ssh://git@github.com/.insteadOf https://github.com/
