d29e606a99d405f3b8dadbf6710ed8596e9dbaf8
Matthew de Rooy <matt@derooy.com.au>
2017-06-05 15:43:06 +1000

    writable: and remove golang

--- install.bash
+++ install.bash
@@ -11,9 +11,9 @@
 # 10. url.ssh://git@github.com/.insteadOf https://github.com/ (don't do this because it messes with anonymous git clones)
 # -----------------------------------------------
 # settings
-plugins=(alias-completion base dirs git go hub nvm rbenv ssh)
+plugins=(alias-completion base dirs git hub nvm rbenv ssh)
 aliases=()
-completions=(defaults git ssh bash-it dirs git go gulp hub npm nvm ssh)
+completions=(defaults git ssh bash-it dirs git gulp hub npm nvm ssh)
 # -----------------------------------------------
 #install or update of bash-it
 git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it #because not possible to fetch depth=1 safely
---
+++ scripts/derooy-writeable.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd ~/dotfiles
+git remote set-url origin git@github.com:derooy/dotfiles.git
